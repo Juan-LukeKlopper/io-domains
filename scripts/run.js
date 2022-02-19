0ccd78e07c21ed375bee60fae8efb3f3d8299113
Juan-LukeKlopper <85296853+Juan-LukeKlopper@users.noreply.github.com>
2022-02-19 14:35:35 +0200

    Updated run me to display contract balance

--- scripts/run.js
+++ scripts/run.js
@@ -3,20 +3,23 @@ const hre = require("hardhat");
 async function main() {
   const [owner, randomPerson] = await hre.ethers.getSigners();
 
-  const domainContractFactory = await hre.ethers.getContractFactory("IODomains");
-  const domainContract = await domainContractFactory.deploy();
+  const domainContractFactory = await hre.ethers.getContractFactory("DomainNameService");
+  const domainContract = await domainContractFactory.deploy("io");
   await domainContract.deployed();
  
   console.log("IODomain service has been deployed to:", domainContract.address);
   console.log("The contract was deployed by: ", owner.address);
 
-  let txn = await domainContract.register("Luke");
+  let txn = await domainContract.register("Luke", {value: hre.ethers.utils.parseEther('0.7')});
   await txn.wait();
 
   let domainOwner = await domainContract.getAddress("Luke");
   console.log("The owner of new domain: ", domainOwner);
 
-  txn = await domainContract.setRecord("Luke","Hello world applications might not seem like much, but they can lead to exceptional things");
+  const balance = await hre.ethers.provider.getBalance(domainContract.address);
+  console.log("Contract balance:", hre.ethers.utils.formatEther(balance));
+
+  /*txn = await domainContract.setRecord("Luke","Hello world applications might not seem like much, but they can lead to exceptional things");
   await txn.wait();
 
   txn = await domainContract.connect(randomPerson).register("Tyson");
@@ -26,7 +29,7 @@ async function main() {
   await txn.wait();
 
   txn = await domainContract.connect(randomPerson).setRecord("Luke","haha im changing something that isn't mine");
-  await txn.wait();
+  await txn.wait();*/
 }
 
 main()
